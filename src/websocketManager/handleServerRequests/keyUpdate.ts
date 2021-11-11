e7aafd2325bc3a4a8a78ab9f1854d4f9071c43b0
CryptoAlgorithm <64193267+cryptoAlgorithm@users.noreply.github.com>
2021-11-11 23:36:14 +0800

    Verifying new GitHub commit email

--- src/websocketManager/handleServerRequests/keyUpdate.ts
+++ src/websocketManager/handleServerRequests/keyUpdate.ts
@@ -1,4 +1,6 @@
-export default function keyUpdate (payload: object): object | null {
+import { IWSReceivedPayload } from '../types';
+
+export default function keyUpdate (payload: IWSReceivedPayload): object | null {
     if ('request' in payload) {
     }
     return null;
